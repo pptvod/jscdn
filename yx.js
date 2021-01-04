04082335afdd3feb468d2d11e52063e88ef76f4b
pptvod <76093628+pptvod@users.noreply.github.com>
2021-01-04 19:49:12 +0800

    Create yx.js

---
+++ yx.js
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/dankogai/js-base64@3.6.0/base64.js"></script>
+<script type="text/javascript"> 
+var str = Base64.decode("JiMxMTI7JiMxMTI7JiMxMTY7JiMxMTg7JiMzNjsmIzExNjsmIzExNzsmIzExNjsmIzk3OyYjMTA5OyYjOTc7JiMxMDU7JiMxMDg7JiM0NjsmIzk5OyYjMTExOyYjMTA5OyYjMzI7");
+document.write(unescape(str.replace(/\\u/g, '%u')));
+</script>
