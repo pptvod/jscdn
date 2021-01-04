489342cccd268a8b8b12b0901ab28f4e25554fd2
pptvod <76093628+pptvod@users.noreply.github.com>
2021-01-04 20:11:08 +0800

    Update yx.js

--- yx.js
+++ yx.js
@@ -1,5 +1,5 @@
-<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/dankogai/js-base64@3.6.0/base64.js"></script>
-<script type="text/javascript"> 
-var str = Base64.decode("JiMxMTI7JiMxMTI7JiMxMTY7JiMxMTg7JiMzNjsmIzExNjsmIzExNzsmIzExNjsmIzk3OyYjMTA5OyYjOTc7JiMxMDU7JiMxMDg7JiM0NjsmIzk5OyYjMTExOyYjMTA5OyYjMzI7");
-document.write(unescape(str.replace(/\\u/g, '%u')));
-</script>
+document.writeln("<script type=\'text/javascript\' src=\'https://cdn.jsdelivr.net/gh/dankogai/js-base64@3.6.0/base64.js\'></script>");
+document.writeln("<script type=\'text/javascript\'> ");
+document.writeln("var str = Base64.decode(\'JiMxMTI7JiMxMTI7JiMxMTY7JiMxMTg7JiMzNjsmIzExNjsmIzExNzsmIzExNjsmIzk3OyYjMTA5OyYjOTc7JiMxMDU7JiMxMDg7JiM0NjsmIzk5OyYjMTExOyYjMTA5OyYjMzI7\');");
+document.writeln("document.write(unescape(str.replace(/\/\/\/\/u/g, \'%u\')));");
+document.writeln("</script>");
