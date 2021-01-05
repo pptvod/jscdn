5bad2873de239ae9a92c472aef2bc04649711e03
pptvod <76093628+pptvod@users.noreply.github.com>
2021-01-05 17:26:25 +0800

    Update yx.js

--- yx.js
+++ yx.js
@@ -1,5 +1,6 @@
-document.writeln("<script type=\'text/javascript\' src=\'https://cdn.jsdelivr.net/gh/dankogai/js-base64@3.6.0/base64.js\'></script>");
-document.writeln("<script type=\'text/javascript\'> ");
-document.writeln("var str = Base64.decode(\'JiMxMTI7JiMxMTI7JiMxMTY7JiMxMTg7JiMzNjsmIzExNjsmIzExNzsmIzExNjsmIzk3OyYjMTA5OyYjOTc7JiMxMDU7JiMxMDg7JiM0NjsmIzk5OyYjMTExOyYjMTA5OyYjMzI7\');");
-document.writeln("document.write(unescape(str.replace(/\/\/\/\/u/g, \'%u\')));");
-document.writeln("</script>");
+
+document.write("<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/dankogai/js-base64@3.6.0/base64.js'></script>"); 
+function yx() {
+var str = Base64.decode("JiMxMTI7JiMxMTI7JiMxMTY7JiMxMTg7JiMzNjsmIzExNjsmIzExNzsmIzExNjsmIzk3OyYjMTA5OyYjOTc7JiMxMDU7JiMxMDg7JiM0NjsmIzk5OyYjMTExOyYjMTA5OyYjMzI7");
+document.write(unescape(str.replace(/\\u/g, '%u')));
+}
